4e11ed2522bb58b70e9c96858dfa82ce4107524d
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-13 20:31:53 +0300

    edited by 'for challengers'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,34 @@ import org.example.model.enums.*;
 
 public class Main {
     public static void main(String[] args) {
-        Wall wall1 = new Wall("North");
-        Wall wall2 = new Wall("South");
-        Wall wall3 = new Wall("East");
-        Wall wall4 = new Wall("West");
-
+        // Ortak oda parçaları
+        Wall n = new Wall("North");
+        Wall s = new Wall("South");
+        Wall e = new Wall("East");
+        Wall w = new Wall("West");
         Ceiling ceiling = new Ceiling(3, PaintColor.WHITE);
-        Bed bed = new Bed("Modern", 2, 1, 1, 1);
         Lamp lamp = new Lamp(LampType.NEON, true, 60);
         Wardrobe wardrobe = new Wardrobe(100, 200, 80.5);
         Carpet carpet = new Carpet(300, 400, PaintColor.RED);
 
-        Bedroom bedroom = new Bedroom("MyRoom", wall1, wall2, wall3, wall4, ceiling, bed, lamp, wardrobe, carpet);
+        // Yatak odasına özel
+        Bed bed = new Bed("Modern", 2, 1, 1, 1);
 
-        // Test metotları
+        // Bedroom nesnesi (Room üzerinden türetilmiş)
+        Bedroom bedroom = new Bedroom(
+                "MyRoom", n, s, e, w,
+                ceiling, lamp, wardrobe, carpet,
+                bed
+        );
+
+        // Test çağrıları
         bedroom.getBed().make();
         bedroom.getLamp().turnOn();
         bedroom.getWardrobe().add();
         bedroom.getCarpet().lying();
         bedroom.getCeiling().create();
         bedroom.getWall1().create();
+
+        System.out.println("Room name: " + bedroom.getName());
     }
 }
